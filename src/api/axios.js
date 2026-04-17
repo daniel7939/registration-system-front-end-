595307e12195840e888d703ca8ecf67832614746
Daniel Tsegay <danieltsegay7939@gmail.com>
2026-04-17 15:22:47 +0300

    up

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+// Detect if we are running in production or development
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: API_BASE_URL
 });
 
 // INTERCEPTOR: Before every single request goes out, run this function!
@@ -11,7 +14,7 @@ instance.interceptors.request.use((config) => {
     
     // 2. If we have one, glue it to the 'Authorization' Header of the outgoing request!
     if (token) {
-        config.headers['Authorization'] = `Bearer ${token}`; // standard convention
+        config.headers['Authorization'] = `Bearer ${token}`; 
     }
     
     // 3. Send it on its way!
